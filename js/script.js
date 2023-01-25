e46c33ffa2ee5d0ec4960ddab9529176bd691ed2
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-01-25 09:34:31 +0100

    ultimato

--- js/script.js
+++ js/script.js
@@ -3,7 +3,48 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        todos: [
+            {
+                text: 'Fare la spesa',
+                done: false,
+            },
+            {
+                text: 'Suonare la tromba',
+                done: false,
+            },
+            {
+                text: 'Leggere 10 pagine',
+                done: false,
+            },
+        ],
+        newTodo: {
+            text: '',
+            done: false,
+        },
       }
+    },
+
+
+    methods: {
+
+
+
+        addTodo() {
+
+            if(this.newTodo != '') {
+
+                this.todos.push(this.newTodo);
+                this.newTodo = '';
+            }
+        },
+        
+        deleteTodo(index) {
+            this.todos.splice(index, 1);
+        },
+        todoDone(i){
+            this.todos[i].done = !this.todos[i].done ;
+            
+            
+        }
     }
   }).mount('#app')
